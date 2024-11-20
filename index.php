b9811facce41cded7f8235a06f733f65579f50a6
DevaanshuSingh <devaanshu111@gmail.com>
2024-11-20 17:44:41 +0530

    Trying To Understand,

--- index.php
+++ index.php
@@ -13,17 +13,39 @@
 </head>
 
 <body>
+<form action="" method="POST" enctype="multipart/form-data">
+    <input type="file" name="img" id="inpt">
+    <label for="inpt">Enter Image</label>
+    <button type="submit">Upload</button>
+</form>
 
-    <form action="" method="GET">
-        <input type="file" name="img" id="inpt">
-        <label for="inpt">Enter Image</label>
-        <button type="submit">Ok</button>
-    </form>
-    <?php
-    $file = $_POST["img"];
-    echo "<img src='$file' alt='Did Not Got Image From index.html'>";
-    echo "<br>image is: " . $_POST["img"];
-    ?>
+<?php
+if (isset($_FILES["img"]) && $_FILES["img"]["error"] === UPLOAD_ERR_OK) {
+    $fileTmpPath = $_FILES["img"]["tmp_name"];
+    $fileName = $_FILES["img"]["name"];
+    $uploadDir = "uploads/";
+
+    // Ensure the upload directory exists
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0755, true);
+    }
+
+    // Sanitize file name
+    $safeFileName = basename($fileName);
+    $destPath = $uploadDir . $safeFileName;
+
+    // Move file to upload directory
+    if (move_uploaded_file($fileTmpPath, $destPath)) {
+        echo "<img src='$destPath' alt='Uploaded Image'>";
+        echo "<br>Image name is: " . htmlspecialchars($safeFileName);
+    } else {
+        echo "Error moving the uploaded file.";
+    }
+} else {
+    echo "No file uploaded or there was an error.";
+}
+?>
+
 </body>
 
 </html>
